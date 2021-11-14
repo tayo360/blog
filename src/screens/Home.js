dbd36b284fb99f650b799738cad3908a4f2d05d4
tayo360 <tayo7651@gmail.com>
2021-11-14 18:37:37 +0100

    update

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,72 @@
 const Home = {
  render: ()=> {
-  return `<div>home</div>`
+  return `<div>
+   <div class="blog-layout container">
+          <div class="blog">
+            <div class="blog-img">
+              <a href="/#/"><img src="wp-images/Gallery 1.jpg" alt="" /></a>
+            </div>
+            <div class="blog-text">
+              <h2>
+                <a href="/#/">Lorem ipsum dolor sit amet consectetur adipisicing.</a>
+              </h2>
+              <div class="d-flex">
+                <div class="category">App Design</div>
+                <div class="date">26th August 2021</div>
+              </div>
+              <div class="author d-flex">
+                <div class="author-image">
+                  <img src="wp-images/profile.jpg" alt="" />
+                </div>
+                <div class="author-name">tayo</div>
+              </div>
+            </div>
+          </div>
+          <div class="blog">
+            <div class="blog-img">
+              <a href="/#/"><img src="wp-images/Gallery 3.jpg" alt="" /></a>
+            </div>
+            <div class="blog-text">
+              <h2>
+                <a href="/#/">Lorem ipsum dolor sit amet consectetur adipisicing.</a>
+              </h2>
+              <div class="d-flex">
+                <div class="category">App Design</div>
+                <div class="date">26th August 2021</div>
+              </div>
+              <div class="author d-flex">
+                <div class="author-image">
+                  <img src="wp-images/profile.jpg" alt="" />
+                </div>
+                <div class="author-name">tayo</div>
+              </div>
+            </div>
+          </div>
+          <div class="blog">
+            <div class="blog-img">
+              <a href="/#/"><img src="wp-images/Gallery 6.jpg" alt="" /></a>
+            </div>
+            <div class="blog-text">
+              <h2>
+                <a href="/#/">Lorem ipsum dolor sit amet consectetur adipisicing.</a>
+              </h2>
+              <div class="d-flex">
+                <div class="category">App Design</div>
+                <div class="date">26th August 2021</div>
+              </div>
+              <div class="author d-flex">
+                <div class="author-image">
+                  <img src="wp-images/profile.jpg" alt="" />
+                </div>
+                <div class="author-name">tayo</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="load-btn">
+          <button class="load" id="load">load more</button>
+        </div>
+  </div>`
  },
  after_render: ()=> {},
 }
